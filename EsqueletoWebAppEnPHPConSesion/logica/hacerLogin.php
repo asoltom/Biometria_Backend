72f78b5fdd87907ae067fe57138c60c0571090e0
Arnau <clavileno3000@gmail.com>
2023-10-02 11:41:19 +0200

    Versión 0.2.1 - Login
    - Se ha implementado el Login correctamente. Ahora puede diferenciar si el usuario (mediante su email y contraseña) son validos (están dentro de la tabla de usuarios).
    Ver NOTA 1.
    - Se ha implementado un archivo de nombre index.html cuyo propósito por el momento es redireccionar al visitante a la pestaña de Login sin tener que forzar a escribir el path entero

--- EsqueletoWebAppEnPHPConSesion/logica/hacerLogin.php
+++ EsqueletoWebAppEnPHPConSesion/logica/hacerLogin.php
@@ -1,29 +1,44 @@
 <?php
 
-//require 'con_bbdd.php';
-
-
 // ---------------------------------------------------------------
 //
-// email:Texto, password:Texto -> hacerLogin() -> VoF
+// email:Texto, password:N -> hacerLogin() -> VoF
 //
 // ---------------------------------------------------------------
 function hacerLogin( $email, $password ) {
 
+  //echo $email;
+  //echo $password;
+
+  //echo $GLOBALS['email'];
+  //echo $GLOBALS['password'];
+  
   $inc = include("con_bbdd.php");
+
+  //-------------------------------------------------------------------------------------------------------------
+  //Para recoger el contenido de las variables $email y $password debo usar el $GLOBALS['index'], de lo contrario
+  //password se me recogerá como un valor nulo después de iniciar la conexión con la BBDD
+  //-------------------------------------------------------------------------------------------------------------
+
+  //echo $GLOBALS['email'];
+  //echo $GLOBALS['password'];
+
+  //echo $email;
+  //echo $password;
+
+  
 
   if($inc){
     $consulta = "SELECT * FROM usuarios";
     $resultado = mysqli_query($conex,$consulta);
     if($resultado){
+
       while($row = $resultado->fetch_array()){
         
           $bbddEmail = $row['email'];
           $bbddPassword = $row['password'];
 
-          echo $password;
-
-          if(($bbddEmail == $email)&&($bbddPassword == $password)){
+          if(($bbddEmail == $GLOBALS['email'])&&($bbddPassword == $GLOBALS['password'])){
             return true;
           }
       }
@@ -32,4 +47,5 @@ function hacerLogin( $email, $password ) {
 
   return false;
 }
+
 ?>
